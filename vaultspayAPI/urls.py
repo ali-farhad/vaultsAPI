567ff943e16809f0a2687473d8edbf9a06b47e2a
ali-farhad <bc170400041@vu.edu.pk>
2020-10-27 13:05:38 +0500

    12 endpoints ready

--- vaultspayAPI/urls.py
+++ vaultspayAPI/urls.py
@@ -69,10 +69,19 @@ def checkMerchantUserRoleExistence(request):
 #Endpoint # 7
 @api.post("/registration/duplicate-email-check") 
 def isEmailDup(request, email:str):
-    # response = requests.post(f'https://vaultspay.com/api/v1/registration/duplicate-email-check?email={email}', data={'email': 'test@test.com'})
-    # req = requests.post('https://en.wikipedia.org/w/index.php', data = {'search':'Nanotechnology'})
+
     url = f'https://vaultspay.com/api/v1/registration/duplicate-email-check?email={email}'
     payload = {'email': f'{email}'}
+    headers= {}
+    response = requests.request("POST", url, headers=headers, data = payload)
+    mydata = response.json()
+    return {"result": mydata}
+
+#Endpoint # 8
+@api.post("/registration/duplicate-phone-number-check") 
+def isPhoneDup(request, phone:str):
+    url = f'https://vaultspay.com/api/v1/registration/duplicate-phone-number-check?phone={phone}'
+    payload = {'phone': f'{phone}'}
     headers= {}
     response = requests.request("POST", url, headers=headers, data = payload)
     mydata = response.json()
@@ -81,12 +90,11 @@ def isEmailDup(request, email:str):
 
 #APIs garded by User Session
 
+#Endpoint # 8
 @api.get("/get-default-wallet-balance") 
 def getDefaultWalletBalance(request, user_id:int = "", token:str = ""):
     headers = {'Authorization': 'Bearer ' + token}
     response = requests.get(f'https://vaultspay.com/api/v1/get-default-wallet-balance?user_id=12', headers=headers)
-    # if(response.status_code == 500):
-    #     print("something went wrong")
     if(response.status_code == 200):
         mydata = response.json()
         x = mydata["success"]["status"]
@@ -94,7 +102,77 @@ def getDefaultWalletBalance(request, user_id:int = "", token:str = ""):
         return {"result": mydata}
     else:
         return {"error": "something went wrong"}
+
+#Endpoint # 9
+@api.get("/get-default-wallet-balance") 
+def getDefaultWalletBalance(request, user_id:int = "", token:str = ""):
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(f'https://vaultspay.com/api/v1/get-default-wallet-balance?user_id={user_id}', headers=headers)
+    if(response.status_code == 500):
+        print("something went wrong")
+
+    elif(response.status_code == 200):
+        mydata = response.json()
+    
+        return {"result": mydata}
+    else:
+        return {"error": "something went wrong"}
   
+#Endpoint # 10
+@api.get("/get-user-profile") 
+def getUserProfile(request, user_id:int = "", token:str = ""):
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(f'https://vaultspay.com/api/v1/get-user-profile?user_id={user_id}', headers=headers)
+    if(response.status_code == 500):
+        print("something went wrong")
+    
+    elif(response.status_code == 200):
+        mydata = response.json()
+     
+        return {"result": mydata}
+    else:
+        return {"error": "something went wrong"}
+
+#Endpoint # 11
+@api.get("/get-user-specific-details") 
+def getUserSpecificDetails(request, email:str = "", token:str = ""):
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(f'https://vaultspay.com/api/v1/get-user-specific-details?email={email}', headers=headers)
+    if(response.status_code == 500):
+        print("something went wrong")
+    
+    elif(response.status_code == 200):
+        mydata = response.json()
+     
+        return {"result": mydata}
+    else:
+        return {"error": "something went wrong"}
+
+#Endpoint # 12 NOT WORKING 
+@api.post("/profile/duplicate-email-check") 
+def profileDuplicateEmailCheck(request, email:str = "", token:str = ""):
+    url = f'https://vaultspay.com/api/v1/profile/duplicate-email-check?email={email}'
+    payload = {'email': f'{email}'}
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.request("POST", url, headers=headers, data = payload)
+    mydata = response.json()
+    return {"result": mydata}
+
+#Endpoint # 13
+@api.get("/check-processed-by") 
+def checkProcessedBy(request, token:str = ""):
+    headers = {'Authorization': 'Bearer ' + token}
+    response = requests.get(f'https://vaultspay.com/api/v1/check-processed-by', headers=headers)
+    if(response.status_code == 500):
+        print("something went wrong")
+    
+    elif(response.status_code == 200):
+        mydata = response.json()
+     
+        return {"result": mydata}
+    else:
+        return {"error": "something went wrong"} 
+
 
 
 urlpatterns = [
